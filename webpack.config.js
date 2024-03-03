deb4d0290c71eb79974a42e18eaa250e1f773b30
Aleksei <lexa24032@gmail.com>
2024-03-03 12:55:46 +0300

    typescript has been installed and set

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,26 @@
 const path = require('path');
 const { webpack } = require('webpack');
 
-module.exports = {};
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devTool: 'inline-source-map',
+};
