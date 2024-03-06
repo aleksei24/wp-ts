d7e75233d41b8bc5982899436fd2f888972adfc2
Aleksei <lexa24032@gmail.com>
2024-03-06 17:01:39 +0300

    some rules for configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const { webpack } = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
+  mode: mode,
+
   entry: './src/index.ts',
+
+  plugins: [new Dotenv()],
+
   module: {
     rules: [
       {
@@ -23,4 +31,14 @@ module.exports = {
   },
 
   devTool: 'inline-source-map',
+
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: false,
+    port: 8080,
+    liveReload: true,
+    watchFiles: ['src/*.html'],
+  },
 };
