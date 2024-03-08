c361cccc4e285baf17136a999b82762636dbb2f0
Aleksei <lexa24032@gmail.com>
2024-03-08 16:24:18 +0300

    styles are linked

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// styles
+import './styles/default.scss';
+import './styles/mixins.scss';
+import './styles/view.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
 
