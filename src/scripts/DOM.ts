c5478bf718e6e36ed2d2d49bfe6b3cf487bec053
Aleksei <lexa24032@gmail.com>
2024-04-02 17:05:27 +0300

    new file tsconfig has been created and set

--- src/scripts/DOM.ts
+++ src/scripts/DOM.ts
@@ -1,8 +1,6 @@
 const printResult = document.querySelector('#log_two') as HTMLElement
 const res: string = 'Hello from DOM.ts'
-const thisYear: string = new Date().getFullYear().toString()
-printResult.setAttribute('datetime', thisYear)
 
 printResult.innerText = res
-printResult.textContent = thisYear
+
 
