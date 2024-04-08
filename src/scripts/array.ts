08aac47b2135a369dc96d4b5ca5d4ccdafd69b3f
Aleksei <lexa24032@gmail.com>
2024-04-08 09:12:45 +0300

    working on array of strings

--- src/scripts/array.ts
+++ src/scripts/array.ts
@@ -1,5 +1,12 @@
-const fruit: string[] = []
-fruit.push('grapes')
+const myArr: string[] = ['grapes', 'lemon']
+myArr.unshift('ARRAY: ')
 
 const printArr = document.querySelector('#arr') as HTMLElement
-printArr.textContent = `${fruit}`
+// printArr.textContent = `${myArr}`
+for (const item of myArr) {
+  const text = document.createElement('span')
+  text.textContent = item
+  printArr.appendChild(text)
+}
+
+myArr.join('')
