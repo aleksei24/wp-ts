1298671dd0e6a4a390215bc8ecf0d350a36543f7
Aleksei <lexa24032@gmail.com>
2024-04-15 09:20:48 +0300

    made a function

--- src/scripts/promise.ts
+++ src/scripts/promise.ts
@@ -5,3 +5,12 @@ interface User {
   email: string
 }
 
+const FetchUsers = async (): Promise<User[]> => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+    return res.json().catch(err => {
+      if (err instanceof Error)
+        console.log(err.message)
+    })
+  })
+  return data
+} 
