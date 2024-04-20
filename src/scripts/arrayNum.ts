944194314008f20b7b3e64c647cfa31be3505656
Aleksei <lexa24032@gmail.com>
2024-04-20 10:17:55 +0300

    tryed method map

--- src/scripts/arrayNum.ts
+++ src/scripts/arrayNum.ts
@@ -1,8 +1,13 @@
-const myNumArray: number[] = [48, 6, 5, 894, 44, 3, 56, 9, 57, 466, 49, 57, 34, 67]
+const myNumArray: number[] = [48, 6, 5, 894, 44, 3, 56, 9, 57, 466, 49, 57, 34]
 const printNumArr = document.querySelector('#arrNum') as HTMLElement
 
 myNumArray.forEach(el => {
   const content = document.createElement('span')
-  content.textContent = `${el.toString()} `
+  content.textContent = `${el} `
   printNumArr.appendChild(content)
 });
+
+const arrMap = myNumArray.filter(n => n > 10).join(' ')
+const printRes = document.createElement('p')
+printRes.textContent = `mapped Arr: ${arrMap}`
+printNumArr.append(printRes)
