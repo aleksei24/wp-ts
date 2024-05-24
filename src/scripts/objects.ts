3fb04dd068212c529fdd2e2acf08cafd2cd0ee3f
Aleksei <lexa24032@gmail.com>
2024-05-24 11:45:28 +0300

    some optimization

--- src/scripts/objects.ts
+++ src/scripts/objects.ts
@@ -3,14 +3,14 @@ const printObject = document.querySelector('#obj') as HTMLObjectElement
 interface Customer {
   name: string,
   age: number,
-  visitDaily: boolean
+  visitDaily?: boolean
 }
 
 function createCustomer(customer: Customer) {
   return `${customer.name}, ${customer.age}, ${customer.visitDaily}`
 }
 
-const customer_one = createCustomer({ name: 'Bill', age: 45, visitDaily: false })
+const customer_one = createCustomer({ name: 'Bill', age: 45, })
 
 const printObjOne = document.createElement('span')
 printObjOne.textContent = `Customer 1: ${customer_one}`
